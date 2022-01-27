4255d093c25dba2022bca2e19e18e275feb26eab
bdoubleu86 <44776308+bdoubleu86@users.noreply.github.com>
2022-01-27 15:31:00 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1240
 iptables -A FORWARD -i tun0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i ens192 -o tun0 -p tcp --syn --dport 44158 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -i ens192 -o tun0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 44158 -j DNAT --to-destination 10.8.0.2
+iptables -t nat -A PREROUTING -i ens192 -p tcp --dport 44158 -j DNAT --to-destination 10.8.0.2
 
 echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
 
