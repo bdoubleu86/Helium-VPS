ed270768907c7fee5dda2afd1fc078ab5ed15cb5
bdoubleu86 <44776308+bdoubleu86@users.noreply.github.com>
2022-01-28 15:55:09 +0100

    Update install-rut.sh

--- install-rut.sh
+++ install-rut.sh
@@ -14,10 +14,10 @@ export PASS="1"
 ./openvpn-install.sh
 
 iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1240
-iptables -A FORWARD -i tun0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -A FORWARD -i eth0 -o tun0 -p tcp --syn --dport 44158 -m conntrack --ctstate NEW -j ACCEPT
-iptables -A FORWARD -i eth0 -o tun0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 44158 -j DNAT --to-destination 10.8.0.2
+iptables -A FORWARD -i tun0 -j ACCEPT; iptables -t nat -A POSTROUTING -o ens192 -j MASQUERADE
+iptables -A FORWARD -i ens192 -o tun0 -p tcp --syn --dport 44158 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -i ens192 -o tun0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -t nat -A PREROUTING -i ens192 -p tcp --dport 44158 -j DNAT --to-destination 10.8.0.2
 
 echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
 
