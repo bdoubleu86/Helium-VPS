91045b2f822382125e6dd1fb2f0d76f170bce380
bdoubleu86 <44776308+bdoubleu86@users.noreply.github.com>
2022-02-02 00:18:41 +0100

    Update check_inet.sh

--- check_inet.sh
+++ check_inet.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 
 UPTIME_LOG=/root/check_inet_uptimecount.log
+UPTIME_TMP=/root/check_inet_uptimecount.tmp
 REBOOT_LOG=/root/check_inet_reboots.log
 
 not_inet_action() {
- sleep 70 && touch /etc/banner && date >> $REBOOT_LOG && reboot
+ oldnum=`cut -d ',' -f2 $UPTIME_TMP`
+ echo "$(date +%Y-%m-%d-%H-%M-%S): $oldnum this is equal to an uptime of $((oldnum*5)) minutes" >> $REBOOT_LOG
+ sleep 70 && touch /etc/banner && reboot
+}
+
+reset_uptimecount() {
+ cp $UPTIME_LOG $UPTIME_TMP
+ echo 0 > $UPTIME_LOG
 }
 
 if ping -c5 10.8.0.1; then
+ newnum=1
  if test -f "$UPTIME_LOG"; then
   oldnum=`cut -d ',' -f2 $UPTIME_LOG`
   newnum=`expr $oldnum + 1`
-  sed -i "s/$oldnum\$/$newnum/g" $UPTIME_LOG
- else
-  echo 1 > $UPTIME_LOG
  fi
+  echo newnum > $UPTIME_LOG
 else
- [[ `cat $UPTIME_LOG` == 0 ]] && no_inet_action || echo 0 > $UPTIME_LOG
+ [[ `cat $UPTIME_LOG` == 0 ]] && no_inet_action || reset_uptimecount
 fi
 
 exit 0
