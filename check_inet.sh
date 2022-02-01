e341b9bf2e0e02e97857e63111cd94bb64fb8f60
bdoubleu86 <44776308+bdoubleu86@users.noreply.github.com>
2022-02-01 23:41:48 +0100

    Update check_inet.sh

--- check_inet.sh
+++ check_inet.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 
-TMP_FILE=/tmp/inet_up
+UPTIME_LOG=/root/check_inet_uptimecount.log
+REBOOT_LOG=/root/check_inet_reboots.log
 
 not_inet_action() {
- sleep 70 && touch /etc/banner && date >> /root/reboot_log && reboot
+ sleep 70 && touch /etc/banner && date >> $REBOOT_LOG && reboot
 }
 
 if ping -c5 10.8.0.1; then
- echo 1 > $TMP_FILE
+ if test -f "$UPTIME_LOG"; then
+  oldnum=`cut -d ',' -f2 $UPTIME_LOG`
+  newnum=`expr $oldnum + 1`
+  sed -i "s/$oldnum\$/$newnum/g" $UPTIME_LOG
+ else
+  echo 1 > $UPTIME_LOG
+ fi
 else
- [[ `cat $TMP_FILE` == 0 ]] && no_inet_action || echo 0 > $TMP_FILE
+ [[ `cat $UPTIME_LOG` == 0 ]] && no_inet_action || echo 0 > $UPTIME_LOG
 fi
 
 exit 0
