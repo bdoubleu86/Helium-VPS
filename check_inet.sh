1860a2f1147bb6b79004327452d91623b6709cca
bdoubleu86 <44776308+bdoubleu86@users.noreply.github.com>
2022-02-01 23:08:54 +0100

    Create check_inet.sh

---
+++ check_inet.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+TMP_FILE=/tmp/inet_up
+
+not_inet_action() {
+ sleep 70 && touch /etc/banner && reboot
+}
+
+if ping -c5 10.8.0.1; then
+ echo 1 > $TMP_FILE
+else
+ [[ `cat $TMP_FILE` == 0 ]] && no_inet_action || echo 0 > $TMP_FILE
+fi
+
+exit 0
