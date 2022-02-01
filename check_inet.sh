e3b90325a4a248516e83f0dc6be821d8523fcf20
bdoubleu86 <44776308+bdoubleu86@users.noreply.github.com>
2022-02-02 00:33:44 +0100

    Update check_inet.sh

--- check_inet.sh
+++ check_inet.sh
@@ -1,29 +1,32 @@
 #!/bin/sh
 
+UPTIME_CNT=/root/check_inet_uptimecount.cnt
 UPTIME_LOG=/root/check_inet_uptimecount.log
-UPTIME_TMP=/root/check_inet_uptimecount.tmp
-REBOOT_LOG=/root/check_inet_reboots.log
+MAX_LINES=10000
+TARGET=10.8.0.1
+
+PREFIX="$(date +%Y-%m-%d-%H-%M-%S): "
 
 not_inet_action() {
- oldnum=`cut -d ',' -f2 $UPTIME_TMP`
- echo "$(date +%Y-%m-%d-%H-%M-%S): $oldnum this is equal to an uptime of $((oldnum*5)) minutes" >> $REBOOT_LOG
  sleep 70 && touch /etc/banner && reboot
 }
 
-reset_uptimecount() {
- cp $UPTIME_LOG $UPTIME_TMP
- echo 0 > $UPTIME_LOG
-}
+#Rotate the logfile if MAX_LINES is reached
+if ((`wc -l < $UPTIME_LOG` > $MAX_LINES)) then
+ mv -f $UPTIME_LOG "$(UPTIME_LOG)1"
+fi
 
-if ping -c5 10.8.0.1; then
+if ping -c5 $TARGET; then
  newnum=1
- if test -f "$UPTIME_LOG"; then
-  oldnum=`cut -d ',' -f2 $UPTIME_LOG`
+ if test -f "$UPTIME_CNT"; then
+  oldnum=`cut -d ',' -f2 $UPTIME_CNT`
   newnum=`expr $oldnum + 1`
  fi
-  echo newnum > $UPTIME_LOG
+ echo newnum > $UPTIME_CNT
+ echo "$(PREFIX) Ping to $TARGET successful" >> $UPTIME_LOG
 else
- [[ `cat $UPTIME_LOG` == 0 ]] && no_inet_action || reset_uptimecount
+ echo "$(PREFIX) Ping to $TARGET failed" >> $UPTIME_LOG
+ [[ `cat $UPTIME_CNT` == 0 ]] && no_inet_action || echo 0 > $UPTIME_CNT
 fi
 
 exit 0
